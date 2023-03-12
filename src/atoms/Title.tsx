42f70818c9f359565554dee901b64da1df92544f
kevortega <kevortega.dev@gmail.com>
2023-03-12 16:09:25 -0300

    add: title big and small

--- src/atoms/Title.tsx
+++ src/atoms/Title.tsx
@@ -6,6 +6,8 @@ type TitleProps = {
 export default function Title({ type = "bigger", ...props }: TitleProps) {
 	const title_types: Record<string, Function> = {
 		bigger: () => <h1 {...props} />,
+		big: () => <h2 {...props} />,
+		medium: () => <h3 {...props} />,
 	};
 
 	return title_types[type]();
