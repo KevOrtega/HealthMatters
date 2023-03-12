3b171eea4e1ddd710f7eca2661b0013fa328bd24
kevortega <kevortega.dev@gmail.com>
2023-03-12 16:05:35 -0300

    add: title types

--- src/atoms/Title.tsx
+++ src/atoms/Title.tsx
@@ -1,7 +1,12 @@
 type TitleProps = {
 	children: React.ReactNode;
+	type?: "bigger" | "big" | "medium";
 };
 
-export default function Title(props: TitleProps) {
-	return <h1 {...props} />;
+export default function Title({ type = "bigger", ...props }: TitleProps) {
+	const title_types: Record<string, Function> = {
+		bigger: () => <h1 {...props} />,
+	};
+
+	return title_types[type]();
 }
