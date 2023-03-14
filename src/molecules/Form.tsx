cd57c5c3dbe4fe333322b83bfc0395f5f17603e5
Svong <74398777+Hejz-Svong@users.noreply.github.com>
2023-03-14 12:01:37 -0300

    before:rest

--- src/molecules/Form.tsx
+++ src/molecules/Form.tsx
@@ -5,6 +5,8 @@ interface FormState {
 	last_name: string;
 	email: string;
 	password: string;
+	medical_license: number;
+	zip_code: number;
 }
 
 const Form = () => {
@@ -13,6 +15,8 @@ const Form = () => {
 		last_name: "",
 		email: "",
 		password: "",
+		medical_license: 0,
+		zip_code: 0,
 	});
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -22,17 +26,69 @@ const Form = () => {
 
 	return (
 		<form>
-			<label htmlFor="Nombre"></label>
-			<input type="text" name="first_name" value={formState.first_name} onChange={handleInputChange} />
+			<label htmlFor="Nombre"> Nombre: </label>
+			<input
+				type="text"
+				name="first_name"
+				value={formState.first_name}
+				placeholder={"Ex: John"}
+				onChange={handleInputChange}
+				required
+			/>
 
-			<label htmlFor="Apellido"></label>
-			<input type="text" />
+			<label htmlFor="Apellido"> Apellido: </label>
+			<input
+				type="text"
+				name="last_name"
+				value={formState.last_name}
+				placeholder={"Ex: Doe"}
+				onChange={handleInputChange}
+				required
+			/>
 
-			<label htmlFor="Email"></label>
-			<input type="text" />
+			<br />
+			<br />
 
-			<label htmlFor="Contraseña"></label>
-			<input type="text" />
+			<label htmlFor="Email"> E-mail: </label>
+			<input
+				type="email"
+				name="email"
+				value={formState.email}
+				placeholder={"Ex: myname@example.com"}
+				onChange={handleInputChange}
+				required
+			/>
+
+			<label htmlFor="Contraseña"> Contraseña: </label>
+			<input
+				type="password"
+				name="password"
+				value={formState.password}
+				placeholder={"********"}
+				onChange={handleInputChange}
+				required
+			/>
+
+			<br />
+			<br />
+
+			<label htmlFor="Medical License">Matricula/Licencia Medica:</label>
+			<input
+				type={"text"}
+				name={"medical_License"}
+				placeholder={"Must be between 6 and 8 characters"}
+				onChange={handleInputChange}
+				required
+			/>
+			<br />
+			<br />
+			<label htmlFor="Zip Code"> Codigo Postal: </label>
+			<input type="text" name="zip_code" placeholder={"Ex: 7600"} onChange={handleInputChange} required />
+
+			<br />
+			<br />
+			<br />
+			<button type="submit">Enviar</button>
 		</form>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Button from "@/atoms/Button";
+import Form from "@/molecules/Form";
 // import Title from "@/atoms/Title";
 
 export default function Home() {
 	return (
 		<main>
 			<Button>first button</Button>
+			<Form></Form>
 		</main>
 	);
 }
