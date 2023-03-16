3a2b9cf5d2e1972444b32f0dd9f2ce411955db8b
Antonellarin <antonellaarin97@gmail.com>
2023-03-16 11:49:53 -0300

    add: Filter,Search,Header

--- src/molecules/Header.tsx
+++ src/molecules/Header.tsx
@@ -1,18 +1,24 @@
 function Header() {
 	return (
-		<div className="header">
-			<h1>HealthMatters</h1>
-			<nav>
-				<ul>
-					<li>
-						<a href="/">Home</a>
-					</li>
-					<li>
-						<a href="/login">LogIn</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
+		<header className="bg-gray-900">
+			<div className="container mx-auto flex justify-between items-center py-4">
+				<h1 className="text-white text-2xl font-bold">HealthMatters</h1>
+				<nav>
+					<ul className="flex space-x-4">
+						<li>
+							<a href="/" className="text-gray-300 hover:text-white">
+								Home
+							</a>
+						</li>
+						<li>
+							<a href="/login" className="text-gray-300 hover:text-white">
+								LogIn
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
 	);
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 //import Button from "@/atoms/Button";
 // import Title from "@/atoms/Title";
-import Header from "@/molecules/Header";
+//import Header from "@/molecules/Header";
+//import SearchBar from "@/molecules/Search";
+import Filter from "@/molecules/Filter";
 
 export default function Home() {
 	return (
 		<main>
-			<Header />
+			<Filter />
 		</main>
 	);
 }
