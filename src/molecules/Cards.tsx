a5e6c045894092d1901cf0568a901f0eed7c89e6
kevortega <kevortega.dev@gmail.com>
2023-03-17 06:23:21 -0300

    modified: cards data into array

--- src/molecules/Cards.tsx
+++ src/molecules/Cards.tsx
@@ -2,64 +2,33 @@ import React from "react";
 import Card from "./Card";
 
 const Cards: React.FC = () => {
+	const cards_data = [
+		{
+			title: "Revision Pediatrica",
+			info: "atención médica de bebés, niños y adolescentes. Hasta los 18 años.",
+			price: 5000,
+			doctor: "John Doe",
+		},
+		{
+			title: "Revision Pediatrica",
+			info: "atención médica de bebés, niños y adolescentes. Hasta los 18 años.",
+			price: 5000,
+			doctor: "John Doe",
+		},
+		{
+			title: "Revision Pediatrica",
+			info: "atención médica de bebés, niños y adolescentes. Hasta los 18 años.",
+			price: 5000,
+			doctor: "John Doe",
+		},
+	];
 	return (
 		<div className="container mx-auto">
 			<h1 className="text-2xl font-bold mb-4">Tarjetas de información</h1>
 			<div className="grid grid-cols-3 gap-4">
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
-				<Card
-					title="Revision Pediatrica"
-					info="atención médica de bebés, niños y adolescentes. Hasta los 18 años."
-					price={5000}
-					doctor="John Doe"
-				/>
+				{cards_data.map((card) => (
+					<Card {...card} />
+				))}
 			</div>
 		</div>
 	);
