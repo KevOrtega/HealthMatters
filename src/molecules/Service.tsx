0a7e7003fb1854e10bb8ee151c6fc6a108e9f00d
kevortega <kevortega.dev@gmail.com>
2023-03-20 13:54:17 -0300

    add: doctor name request

--- src/molecules/Service.tsx
+++ src/molecules/Service.tsx
@@ -4,6 +4,7 @@ import { serviceProps } from "@/interface";
 
 import Button from "@/atoms/Button";
 import Title from "@/atoms/Title";
+import useDoctorById from "@/hooks/useDoctorById";
 
 const Service: React.FC<serviceProps> = ({
 	className,
@@ -13,6 +14,8 @@ const Service: React.FC<serviceProps> = ({
 	doctor,
 	rating,
 }) => {
+	const { service_doctor } = useDoctorById(doctor);
+
 	return (
 		<Button
 			className={
@@ -26,9 +29,11 @@ const Service: React.FC<serviceProps> = ({
 			<p className="text-egg my-2 text-left">{description}</p>
 			<p className="text-egg mb-2 text-left">${price}</p>
 			<p className="text-egg mb-2 text-left">rating: {rating}</p>
-			<p className="z-10 absolute bottom-0 right-0 min-w-max w-2/5 translate-x-1/3 translate-y-1/3 bg-deep-sea text-white uppercase text-lg text-center p-4">
-				{doctor.length ? doctor : "doctor gutierrez"}
-			</p>
+			{service_doctor && (
+				<p className="z-10 absolute bottom-0 right-0 min-w-max w-2/5 translate-x-1/3 translate-y-1/3 bg-deep-sea text-white uppercase text-lg text-center p-4">
+					{service_doctor.name} {service_doctor.lastname}
+				</p>
+			)}
 		</Button>
 	);
 };
--- src/requests.ts
+++ src/requests.ts
@@ -1,4 +1,8 @@
-import { services_response, specialties_response } from "@/interface";
+import {
+	doctor_response,
+	services_response,
+	specialties_response,
+} from "@/interface";
 import { Fetcher } from "swr";
 import axios, { AxiosResponse } from "axios";
 
@@ -11,3 +15,6 @@ export const specialtiesFetcher: Fetcher<specialties_response[], string> = (
 	axios
 		.get(url)
 		.then(({ data }: AxiosResponse<specialties_response[]>) => data);
+
+export const doctorByIdFetcher: Fetcher<doctor_response, string> = (url) =>
+	axios.get(url).then(({ data }: AxiosResponse<doctor_response>) => data);
