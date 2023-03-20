9aad147cbf5b5c067d91c6f2c13af358c676474a
Svong <74398777+Hejz-Svong@users.noreply.github.com>
2023-03-20 04:55:34 -0300

    Style

--- src/molecules/DoctorForm.tsx
+++ src/molecules/DoctorForm.tsx
@@ -12,13 +12,22 @@ import {
 export default function DoctorForm() {
 	return (
 		<div>
-			<form>
-				DoctorForm
-				<FirstNameInput /> <LastNameInput />
-				<PasswordInput /> <EmailInput />
-				<LicenseInput /> <SpecialtyInput />
-				<ZipCodeInput /> <PhoneInput />
-				<button type="submit">Enviar</button>
+			<form className="container py-1">
+				<div className="mt-4 flex justify-end">
+					<div className="w-1/2 text-center">
+						<h2 className="text-2xl font-bold mb-4">Doctor Form</h2>
+						<FirstNameInput /> <LastNameInput />
+						<PasswordInput /> <EmailInput />
+						<LicenseInput /> <SpecialtyInput />
+						<ZipCodeInput /> <PhoneInput />
+						<button
+							type="submit"
+							className="ml-2 text-sm text-gray-500 hover:text-gray-700"
+						>
+							Send
+						</button>
+					</div>
+				</div>
 			</form>
 		</div>
 	);
