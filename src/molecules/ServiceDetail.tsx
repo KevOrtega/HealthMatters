b33009a8bedbe84266761d17a5d940d8ac35feeb
Antonellarin <antonellaarin97@gmail.com>
2023-03-25 16:14:45 -0300

    add mercado pago

--- src/molecules/ServiceDetail.tsx
+++ src/molecules/ServiceDetail.tsx
@@ -1,26 +1,68 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useServicesById } from "@/hooks/useServicesById";
 import { useCartContext } from "@/context/CartProvider";
 import axios from "axios";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
 
 export default function ServiceDetail({ serviceId }: { serviceId: string }) {
 	const { service } = useServicesById(serviceId);
 	const { addToCart, cartItems } = useCartContext();
 
+	useEffect(() => {
+		if (service) {
+			const script = document.createElement("script");
+			script.type = "text/javascript";
+			script.src = "https://sdk.mercadopago.com/js/v2";
+			script.setAttribute(
+				"data-preference-id",
+				service.preferenceId.toString()
+			);
+			document.body.appendChild(script);
+
+			script.onload = () => {
+				const mp = new window.MercadoPago(
+					publicRuntimeConfig.NEXT_PUBLIC_MP_PUBLIC_KEY,
+					{
+						locale: "es-AR",
+					}
+				);
+
+				const checkoutButton = document.getElementById("checkout-button");
+				if (checkoutButton) {
+					checkoutButton.addEventListener("click", () => {
+						mp.checkout({
+							preference: {
+								id: service.preferenceId,
+							},
+							render: {
+								container: ".cho-container",
+								label: "Pagar",
+							},
+						});
+					});
+				}
+			};
+		}
+	}, [service]);
+
 	const handleClick: React.ReactEventHandler<HTMLButtonElement> = () =>
 		service && addToCart(service);
 
 	const handleBought: React.ReactEventHandler<HTMLButtonElement> = async () => {
-		await axios.post(process.env.checkout_url || "", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				price: cartItems[0].services.price,
-				patient: "paciente",
-			}),
-		});
+		if (service) {
+			await axios.post(publicRuntimeConfig.checkout_url || "", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					price: service.price,
+					patient: "paciente",
+				}),
+			});
+		}
 	};
 
 	return (
@@ -29,7 +71,7 @@ export default function ServiceDetail({ serviceId }: { serviceId: string }) {
 				<>
 					<h1>{service.name}</h1>
 					<button onClick={handleClick}>add service to my services</button>
-					<button onClick={handleBought}>buy</button>
+					<button id="checkout-button">buy</button>
 				</>
 			)}
 			<p>Servicios en el carrito: {cartItems.length}</p>
