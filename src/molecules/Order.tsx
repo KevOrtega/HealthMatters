ce3967de4a60440e39d97efbd027081be4babb45
Romina <fattorecitter@gmail.com>
2023-03-28 12:41:45 -0300

    Order : alphabetical

--- src/molecules/Order.tsx
+++ src/molecules/Order.tsx
@@ -10,7 +10,7 @@ export default function Order() {
 	const dispatchOrder = (order_selected: string) =>
 		dispatch({ type: "SET_ORDER", payload: order_selected });
 
-	const setOrder = (order_selected: "price" | "rating") => {
+	const setOrder = (order_selected: "price" | "rating" | "alphabetical") => {
 		const change_order_methods = {
 			price: () =>
 				order !== "priceASC" && order !== "priceDESC"
@@ -24,43 +24,68 @@ export default function Order() {
 					: order === "ratingASC"
 					? dispatchOrder("ratingDESC")
 					: dispatchOrder("default"),
+			alphabetical: () =>
+				order !== "alphabeticallyASC" && order !== "alphabeticallyDESC"
+					? dispatchOrder("alphabeticallyASC")
+					: order === "alphabeticallyASC"
+					? dispatchOrder("alphabeticallyDESC")
+					: dispatchOrder("default"),
 		};
 
 		change_order_methods[order_selected]();
+		//console.log('order:', order);
 	};
 
 	return (
-		<div className="text-mine-shaft bg-white shadow-xl min-w-max rounded-lg w-72 overflow-hidden">
+		<div className="text-mine-shaft bg-white shadow-xl min-w-max rounded-lg w-72 overflow-hidden flex flex-col items-center">
 			<Title className="p-5" type="medium">
 				Order by
 			</Title>
+			<div className="flex w-full">
+				<Button
+					onClick={() => setOrder("price")}
+					className={`inline-flex justify-center items-center w-1/2 h-16 p-5 rounded-tr-lg font-bold capitalize transition-colors ${
+						order.includes("price") && "bg-anakiwa text-white"
+					}`}
+				>
+					price
+					{order.includes("price") && (
+						<Image
+							className={`text-white h-7 ${
+								order === "priceDESC" && "rotate-180"
+							} `}
+							type="ascendant"
+						/>
+					)}
+				</Button>
+				<Button
+					onClick={() => setOrder("rating")}
+					className={`inline-flex justify-center items-center w-1/2 h-16 p-5 rounded-tl-lg font-bold capitalize transition-colors ${
+						order.includes("rating") && "bg-anakiwa text-white"
+					}`}
+				>
+					rating
+					{order.includes("rating") && (
+						<Image
+							className={`text-white h-7 ${
+								order === "ratingDESC" && "rotate-180"
+							} `}
+							type="ascendant"
+						/>
+					)}
+				</Button>
+			</div>
 			<Button
-				onClick={() => setOrder("price")}
-				className={`inline-flex justify-center items-center w-1/2 h-16 p-5 rounded-tr-lg rounded-bl-lg font-bold capitalize transition-colors ${
-					order.includes("price") && "bg-anakiwa text-white"
+				onClick={() => setOrder("alphabetical")}
+				className={`inline-flex justify-center items-center w-1/2 h-16 p-5 mt-4 rounded-lg font-bold capitalize transition-colors ${
+					order.includes("alphabetical") && "bg-anakiwa text-white"
 				}`}
 			>
-				price
-				{order.includes("price") && (
+				alphabetical
+				{order.includes("alphabetical") && (
 					<Image
 						className={`text-white h-7 ${
-							order === "priceDESC" && "rotate-180"
-						} `}
-						type="ascendant"
-					/>
-				)}
-			</Button>
-			<Button
-				onClick={() => setOrder("rating")}
-				className={`inline-flex justify-center items-center w-1/2 h-16 p-5 rounded-tl-lg rounded-br-lg font-bold capitalize transition-colors ${
-					order.includes("rating") && "bg-anakiwa text-white"
-				}`}
-			>
-				rating
-				{order.includes("rating") && (
-					<Image
-						className={`text-white h-7 ${
-							order === "ratingDESC" && "rotate-180"
+							order === "alphabeticalDESC" && "rotate-180"
 						} `}
 						type="ascendant"
 					/>
