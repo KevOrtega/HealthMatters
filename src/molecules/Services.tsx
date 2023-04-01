9d2cc3f779a144577fc45a9afa7c615ca85e67f8
Antonellarin <antonellaarin97@gmail.com>
2023-04-01 15:33:40 -0300

    add service loading

--- src/molecules/Services.tsx
+++ src/molecules/Services.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import useSWR from "swr";
 import Service from "@/molecules/Service";
-import useServices from "@/hooks/useServices";
 import Title from "@/atoms/Title";
 import { useServiceSearchContext } from "@/context/ServiceSearchProvider";
 import Pagination from "@/molecules/Pagination";
 import { iService } from "@/interface";
+import useServices from "@/hooks/useServices";
 
 export default function Services() {
 	const { search, specialties, order, page } = useServiceSearchContext();
-	const { services } = useServices(search, specialties, order, page);
+	const { services, isLoading } = useServices(search, specialties, order, page);
+
+	if (isLoading) {
+		return (
+			<div className="flex items-center justify-center h-screen">
+				<div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+			</div>
+		);
+	}
+
+	if (!services || services.length === 0) {
+		return (
+			<div className="flex items-center justify-center h-screen">
+				<h1 className="text-3xl font-bold text-gray-900">
+					No se encontraron servicios
+				</h1>
+			</div>
+		);
+	}
 
 	return (
 		<div className="w-full px-20">
-			<Title className="text-left my-6" type="medium">
-				Services
+			<Title className="text-left my-6 text-2xl font-bold text-gray-900">
+				Servicios
 			</Title>
 			<Pagination />
 			<div className="w-full flex justify-evenly items-start flex-wrap">
