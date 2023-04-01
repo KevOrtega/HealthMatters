19944383fec3964049a260c2d00e4a4672a8e484
Romina <fattorecitter@gmail.com>
2023-04-01 11:01:50 -0300

    DoctorProfileForm

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
 		register_url:
 			"https://healthmattersapi-production.up.railway.app/auth/register",
 		checkout_url: "https://healthmattersapi-production.up.railway.app/checkout",
+		validate_doctor_url:
+			"https://healthmattersapi-production.up.railway.app/order",
+		// services_url: "http://localhost:3001/services",
+		// specialties_url: "http://localhost:3001/specialties",
+		// doctors_url: "http://localhost:3001/doctors",
+		// login_url: "http://localhost:3001/auth/login",
+		// register_url: "http://localhost:3001/auth/register",
+		// checkout_url: "http://localhost:3001/checkout",
+		// validate_doctor_url: "http://localhost:3001/order",
 	},
 };
 
